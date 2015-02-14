2f7e26ce42a3075521ba2c95b56f09e753dcb15a
Nickolai Novik <is.infinity@yahoo.com>
2015-02-14 19:00:04 +0200

    add errors definition

---
+++ aiogearman/errors.py
@@ -0,0 +1,40 @@
+from .consts import ERROR, WORK_FAIL, WORK_EXCEPTION
+
+
+class GearmanException(Exception):
+    """Rises whenever the server encounters an error and needs
+    to notify a client or worker"""
+
+    code = ERROR
+
+class GearmanWorkFailException(GearmanException):
+    """This is to notify the server (and any listening clients) that
+    the job failed.
+
+    """
+
+    code = WORK_FAIL
+
+
+class GearmanWorkException(GearmanException):
+    """WORK_EXCEPTION
+
+    This is to notify the server (and any listening clients) that
+    the job failed with the given exception.
+
+    Arguments:
+    - NULL byte terminated job handle.
+    - Opaque data that is returned to the client as an exception."""
+
+    code = WORK_EXCEPTION
+
+
+errors_map = {
+    ERROR: GearmanException,
+    WORK_FAIL: GearmanWorkFailException,
+    WORK_EXCEPTION: GearmanWorkException
+}
+
+
+def heck_error(code):
+    return errors_map.get(code, None)
