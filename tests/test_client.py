c35d82b108ebf1709f54e25ec3ced15ba011d59c
Nickolai Novik <is.infinity@yahoo.com>
2015-02-27 00:05:00 +0200

    make unique_id different in tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -7,12 +7,12 @@ class ClientTest(GearmanTest):
     @run_until_complete
     def test_client_ctor(self):
         client = yield from create_client(loop=self.loop)
-        job = yield from client.submit(b'rev', b'submit', unique_id=123)
+        job = yield from client.submit(b'rev', b'submit', unique_id=1)
 
         self.assertEqual(job.function, b'rev')
         self.assertEqual(job.data, b'submit')
-        self.assertEqual(job.unique_id, 123)
-        self.assertTrue(job.job_id, 123)
+        self.assertEqual(job.unique_id, 1)
+        self.assertTrue(job.job_id, 3)
 
         yield from self.worker.do_job()
 
@@ -26,7 +26,7 @@ class ClientTest(GearmanTest):
     def test_client_submit_high(self):
         client = yield from create_client(loop=self.loop)
         job = yield from client.submit_high(b'rev', b'submit_high',
-                                            unique_id=123)
+                                            unique_id=4)
         yield from self.worker.do_job()
         result = yield from job.wait_result()
         self.assertEqual(result, b'hgih_timbus')
@@ -35,7 +35,7 @@ class ClientTest(GearmanTest):
     def test_client_submit_low(self):
         client = yield from create_client(loop=self.loop)
         job = yield from client.submit_low(b'rev', b'submit_low',
-                                           unique_id=123)
+                                           unique_id=5)
         yield from self.worker.do_job()
         result = yield from job.wait_result()
         self.assertEqual(result, b'wol_timbus')
@@ -44,7 +44,7 @@ class ClientTest(GearmanTest):
     def test_client_submit_bg(self):
         client = yield from create_client(loop=self.loop)
         job = yield from client.submit_bg(b'rev', b'submit_bg',
-                                          unique_id=123)
+                                          unique_id=6)
         yield from self.worker.do_job()
         self.assertTrue(job)
 
@@ -52,7 +52,7 @@ class ClientTest(GearmanTest):
     def test_client_submit_high_bg(self):
         client = yield from create_client(loop=self.loop)
         job = yield from client.submit_high_bg(b'rev', b'submit_high_bg',
-                                               unique_id=123)
+                                               unique_id=7)
         yield from self.worker.do_job()
         self.assertTrue(job)
 
@@ -60,6 +60,6 @@ class ClientTest(GearmanTest):
     def test_client_submit_low_bg(self):
         client = yield from create_client(loop=self.loop)
         job = yield from client.submit_low_bg(b'rev', b'submit_low',
-                                              unique_id=123)
+                                              unique_id=8)
         yield from self.worker.do_job()
         self.assertTrue(job)
