5c03a589f3e95d4d23f930638cca4eab5ef2101c
Nickolai Novik <is.infinity@yahoo.com>
2015-03-03 23:59:03 +0200

    add one more test case

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,6 +1,6 @@
 import asyncio
 from ._testutil import BaseTest, run_until_complete
-from aiogearman import create_worker
+from aiogearman import create_worker, GearmanWorkException
 import aiogearman
 
 
@@ -28,3 +28,18 @@ class WorkerTest(BaseTest):
 
         r = yield from job.wait_result()
         self.assertEqual(r, b'oof')
+
+    @run_until_complete
+    def test_do_job_exception(self):
+        client = yield from aiogearman.create_client(loop=self.loop)
+        job = yield from client.submit(b'rev', b'foo')
+        worker = yield from create_worker(loop=self.loop)
+
+        @asyncio.coroutine
+        def rev(data):
+            raise Exception('log story short: error')
+
+        yield from worker.register_function(b'rev', rev)
+        yield from worker.do_job()
+        with self.assertRaises(GearmanWorkException):
+            yield from job.wait_result()
