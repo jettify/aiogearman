6afda80ea4dc389c8aa371a584c3b16461b47c36
Nickolai Novik <is.infinity@yahoo.com>
2015-03-11 23:57:54 +0200

    add tests for canceled futures

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -29,6 +29,28 @@ class ConnectionTest(BaseTest):
         conn.close()
         self.assertEqual(conn.closed, True)
 
+    @run_until_complete
+    def test_connect_close(self):
+        conn = yield from create_connection(loop=self.loop)
+        self.assertTrue(conn._loop, self.loop)
+        resp_fut1 = conn.execute(ECHO_REQ, b'foo')
+        resp_fut2 = conn.execute(ECHO_REQ, b'foo')
+        conn.close()
+        self.assertTrue(resp_fut1.cancelled)
+        self.assertTrue(resp_fut2.cancelled)
+
+    @run_until_complete
+    def test_execute_cancel_future(self):
+        conn = yield from create_connection(loop=self.loop)
+        self.assertTrue(conn._loop, self.loop)
+        resp_fut1 = conn.execute(ECHO_REQ, b'foo')
+        resp_fut1.cancel()
+
+        packet_type, data = yield from conn.execute(ECHO_REQ, b'foo')
+        conn.close()
+        self.assertEqual(packet_type, ECHO_RES)
+        self.assertEqual(data, b'foo')
+
     def test_encode_command(self):
         res = encode_command(REQ, ECHO_RES, 'foo', 3.14)
         expected = b'\x00REQ\x00\x00\x00\x11\x00\x00\x00\x08foo\x003.14'
